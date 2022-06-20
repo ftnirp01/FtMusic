750387b4d19d179a406f9bab9f298a1b84a84eeb
ftnirp01 <ftnirp@naver.com>
2022-06-20 17:44:58 +0900

    프로젝트 생성 완료

---
+++ src/main/java/com/ftnirp/music/FtMusicApplication.java
@@ -0,0 +1,13 @@
+package com.ftnirp.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ftnirp/music/FtMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftnirp.music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
